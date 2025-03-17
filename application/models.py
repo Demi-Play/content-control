40276b04b819704dd165e3f47359585f352b0caf
D3M1.dev <63244338+Demi-Play@users.noreply.github.com>
2025-03-17 16:53:02 +0300

    hot fix auth

--- application/models.py
+++ application/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -19,7 +20,12 @@ class User(db.Model, UserMixin):
     email_notifications = db.Column(db.Boolean, default=True)  # Флаг для email уведомлений
     
     # is_blocked = db.Column(db.Boolean, default=False)
-    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+        
     def is_active(self):
         if self.is_blocked:
             return False
